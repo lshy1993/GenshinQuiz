c0796e91ba137ab927f3e496123555a26fcbf7bd
lshy1993 <lshy1993@live.com>
2024-12-19 04:00:12 +0900

    api

--- frontend/orval.config.mjs
+++ frontend/orval.config.mjs
@@ -2,9 +2,12 @@ export default {
   api: {
     input: './openapi/api-docs.yaml', // OpenAPI YAML 文件路径
     output: {
-      mode: 'tags', // 按 tags 生成文件
+      mode: 'operationId', // 按 operationId 生成文件
       target: './src/api', // 生成的 TypeScript 客户端代码路径
       client: 'axios', // 使用 Axios 作为 HTTP 客户端
+      transformOperationId: (operationId) => {
+        return operationId.replace(/-./g, (match) => match.charAt(1).toUpperCase());
+      }, // 转换操作 ID 为驼峰命名
     },
   },
 };
